20d947a3d93799fa1f35f8bd050cb241784f32fb
Elias Igbalajobi <heeleeaz@gmail.com>
2018-12-08 22:15:43 +0100

    Initial Commit

---
+++ VelaSDK/VelaSDK.h
@@ -0,0 +1,19 @@
+//
+//  VelaSDK.h
+//  VelaSDK
+//
+//  Created by Igbalajobi Elias on 12/8/18.
+//  Copyright © 2018 Igbalajobi Elias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VelaSDK.
+FOUNDATION_EXPORT double VelaSDKVersionNumber;
+
+//! Project version string for VelaSDK.
+FOUNDATION_EXPORT const unsigned char VelaSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VelaSDK/PublicHeader.h>
+
+
